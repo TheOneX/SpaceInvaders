6c619c4edda6df3248ed8cf610bd2ebe93f6a73d
mcabe <mcabe@me.com>
2014-02-19 13:45:45 +0100

    Bullet class

---
+++ src/spaceinvaders/Bullet.java
@@ -0,0 +1,15 @@
+package spaceinvaders;
+
+/**
+ *
+ * @author krismaini
+ */
+public class Bullet extends Thing{
+
+    public Bullet(int posX, int posY, int width, int height, float pixelsPrMiliSec) {
+        super(posX, posY, width, height, pixelsPrMiliSec);
+    }
+    public void moveUp(){
+        this.setPosY(this.getPosY() + 1);
+    }
+}
